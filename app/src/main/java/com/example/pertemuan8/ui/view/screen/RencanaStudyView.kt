b0efd66ef4133461c74a93b31c5bfec8785b792f
luthfibphoenix <lutfibahtiar2409@gmail.com>
2024-11-21 20:03:57 +0700

    membuat kolom pada RencanaStudyView.kt

--- app/src/main/java/com/example/pertemuan8/ui/view/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/pertemuan8/ui/view/screen/RencanaStudyView.kt
@@ -1,10 +1,18 @@
 package com.example.pertemuan8.ui.view.screen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.dp
+import com.example.pertemuan8.R
 import com.example.pertemuan8.model.Mahasiswa
 
 @Composable
@@ -17,4 +25,12 @@ fun RencanaStudiView(
     var checked by remember { mutableStateOf(false) }
     var pilihanKelas by remember { mutableStateOf("") }
     val listData: MutableList<String> = mutableListOf(choseDropdown, pilihanKelas)
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = colorResource(id = R.color.primary))
+    ) {
+
+    }
 }
