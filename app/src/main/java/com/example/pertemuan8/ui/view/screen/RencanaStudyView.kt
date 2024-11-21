193df9b8e72b6923bde08fe612f66d998779b7ea
luthfibphoenix <lutfibahtiar2409@gmail.com>
2024-11-21 20:20:50 +0700

    menambahkan DynamicSelectedTextField

--- app/src/main/java/com/example/pertemuan8/ui/view/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/pertemuan8/ui/view/screen/RencanaStudyView.kt
@@ -2,13 +2,21 @@ package com.example.pertemuan8.ui.view.screen
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -17,11 +25,16 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.pertemuan8.R
+import com.example.pertemuan8.data.MataKuliah
 import com.example.pertemuan8.model.Mahasiswa
+import com.example.pertemuan8.ui.widget.DynamicSelectedTextField
 
 @Composable
 fun RencanaStudiView(
@@ -52,6 +65,49 @@ fun RencanaStudiView(
                     .clip(shape = CircleShape)
                     .size(50.dp)
             )
+            Spacer(modifier = Modifier.padding(start = 16.dp))
+            Column(modifier = Modifier.weight(1f)) {
+                Text(
+                    text = mahasiswa.nama,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp,
+                    color = Color.Yellow
+                )
+                Text(
+                    text = mahasiswa.nim,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp,
+                    color = Color.Yellow
+                )
+            }
+            Icon(
+                imageVector = Icons.Filled.Notifications,
+                contentDescription = null,
+                tint = Color.Yellow
+            )
+        }
+        Box(
+            modifier = Modifier
+                .background(color = Color.White, shape = RoundedCornerShape(topEnd = 15.dp, topStart = 15.dp))
+                .fillMaxSize()
+        ){
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp)
+            ){
+                Text(text = "Pilih Mata Kuliah Peminatan")
+                Text(text = "Silahkan pilih mata kuliah yang anda inginkan")
+                Spacer(modifier = Modifier.height(8.dp))
+                DynamicSelectedTextField(
+                    selectedValue = choseDropdown,
+                    options = MataKuliah.option,
+                    label = "Mata Kuliah",
+                    onValueChangedEvent = {
+                        choseDropdown = it
+                    }
+                )
+            }
         }
     }
 }
