83b064fbdda2607af77ba8d38da0a81eae5fb429
luthfibphoenix <lutfibahtiar2409@gmail.com>
2024-11-21 18:53:43 +0700

    membuat function RencanaStudiView

--- app/src/main/java/com/example/pertemuan8/ui/view/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/pertemuan8/ui/view/screen/RencanaStudyView.kt
@@ -1,2 +1,13 @@
 package com.example.pertemuan8.ui.view.screen
 
+import androidx.compose.runtime.Composable
+import com.example.pertemuan8.model.Mahasiswa
+
+@Composable
+fun RencanaStudiView(
+    mahasiswa: Mahasiswa,
+    onSimpanButtonClicked: (MutableList<String>) -> Unit,
+    onBackButtonClicked: () -> Unit
+){
+
+}
