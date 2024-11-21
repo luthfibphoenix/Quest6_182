532937400ff18696afe78d8cd22a610767d4e0fc
luthfibphoenix <lutfibahtiar2409@gmail.com>
2024-11-21 19:01:06 +0700

    menambahkan ExposedDropdownMenuBox

--- app/src/main/java/com/example/pertemuan8/ui/widget/DynamicSelectedField.kt
+++ app/src/main/java/com/example/pertemuan8/ui/widget/DynamicSelectedField.kt
@@ -1,8 +1,15 @@
 package com.example.pertemuan8.ui.widget
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DynamicSelectedTextField(
     selectedValue: String,
@@ -11,5 +18,13 @@ fun DynamicSelectedTextField(
     onValueChangedEvent: (String) -> Unit,
     modifier: Modifier = Modifier
 ){
+    var expanded by remember { mutableStateOf(false) }
 
+    ExposedDropdownMenuBox(
+        expanded = expanded,
+        onExpandedChange = {expanded = !expanded},
+        modifier = modifier
+    ){
+
+    }
 }
