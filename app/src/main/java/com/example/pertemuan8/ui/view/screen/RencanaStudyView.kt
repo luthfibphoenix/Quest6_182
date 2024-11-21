255659d044a0fc24042217f0f03952f3a86ef84e
luthfibphoenix <lutfibahtiar2409@gmail.com>
2024-11-21 18:58:09 +0700

    membuat function DynamicSelectedTextField

--- app/src/main/java/com/example/pertemuan8/ui/view/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/pertemuan8/ui/view/screen/RencanaStudyView.kt
@@ -1,6 +1,8 @@
 package com.example.pertemuan8.ui.view.screen
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import com.example.pertemuan8.model.Mahasiswa
 
 @Composable
@@ -9,5 +11,10 @@ fun RencanaStudiView(
     onSimpanButtonClicked: (MutableList<String>) -> Unit,
     onBackButtonClicked: () -> Unit
 ){
+    var choseDropdown by remember {
+        mutableStateOf(
+            ""
+        )
+    }
 
 }
--- app/src/main/java/com/example/pertemuan8/ui/widget/DynamicSelectedField.kt
+++ app/src/main/java/com/example/pertemuan8/ui/widget/DynamicSelectedField.kt
@@ -1,2 +1,15 @@
 package com.example.pertemuan8.ui.widget
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun DynamicSelectedTextField(
+    selectedValue: String,
+    options: List<String>,
+    label: String,
+    onValueChangedEvent: (String) -> Unit,
+    modifier: Modifier = Modifier
+){
+
+}
